9da1693b3977862ddd938883af5101f366e31c18
Justin <justin.bimbrahw@gmail.com>
2020-09-17 19:01:16 -0400

    September 17 - added quiz

---
+++ server.js
@@ -0,0 +1,142 @@
+//we are using express to create our server
+//later we used docker to set up a postgres database and we used TablePlus as the GUI to set up the table
+
+//This code defines a number of routes:
+//a get for each of the 3 web pages
+//a get to fetch all the facts from the database
+//a post to send a new fact to the db (create a new record)
+//a delete to delete a record - this is the only request that sends a parameter
+//we also have to specify on what port the app is listening -
+//that establishes the channel that serves as an input to our server
+
+//server
+const express = require('express')
+require('dotenv').config()
+
+//cross origin request...attaching this to our middleware to allow requests coming in from different ports
+const cors = require('cors')
+
+//so we can accept and read JSON easier...helps us to fetch all the facts
+const bodyParser = require('body-parser')
+
+//we are using db.js for our db connection
+const db = require('./db')
+const app = express()
+const port = process.env.PORT || 8080
+
+app.set('view engine', 'ejs')
+app.use(cors())
+app.use(bodyParser())
+app.use(express.static('assets'))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/viewdata', (req, res) => {
+    db.Facts.findAll({
+        order: [
+            ['date', 'ASC'],
+            ['fact', 'ASC']
+        ]
+    }).catch(err => {
+        console.log(err)
+        res.status(500).end()
+    }).then( facts => {
+        console.log(facts)
+        res.render('viewdata', {
+            "facts" : facts     //I tried changing "facts" to something else
+        })                     //it must also be changed accordingly in viewdata.ejs
+    })
+})
+
+app.get('/deletedata', (req, res) => {
+    db.Facts.findAll({
+        order: [
+            ['date', 'ASC'],
+            ['fact', 'ASC']
+        ]
+    }).catch(err => {
+        console.log(err)
+        res.status(500).end()
+    }).then( facts => {
+        console.log(facts)
+        res.render('deletedata', {
+            "facts" : facts
+        })
+    })
+})
+
+app.get('/quiz', (req, res) => {
+    db.Facts.findAll({
+        order: [
+            ['date', 'ASC'],
+            ['fact', 'ASC']
+        ]
+    }).catch(err => {
+        console.log(err)
+        res.status(500).end()
+    }).then( facts => {
+        console.log(facts)
+        res.render('quiz', {
+            "facts" : facts
+        })
+    })
+})
+
+
+app.get('/api', (req, res) => {
+    db.Facts.findAll({
+        order: [
+            ['date', 'ASC'],
+            ['fact', 'ASC']
+        ]
+    }).catch(err => {
+        console.log(err)
+        res.status(500).end()
+    }).then( facts => {
+        console.log(facts)
+        res.json(facts)
+    })
+})
+
+
+//WHAT IS METADATA BELOW?
+//IT LOOKS LIKE THE DB CREATES THE OTHER COLUMN ENTRIES
+//ON ITS OWN
+
+app.post('/api', (req, res) => {
+    console.log(req.body)
+    db.Facts.create({fact: req.body.eventdata, date: req.body.year})
+        .catch(err =>{
+            console.log(err)
+            res.json({success: false})
+        })
+        .then((result, metaData) => { 
+            console.log(result, metaData)
+            res.json({success: true})
+        })
+})
+
+app.delete('/api/:id', (req, res) => {
+    const id = req.params.id
+    db.Facts.destroy({
+        where: { "id": id }
+    }).catch(err => {
+        console.log(err)
+        res.status(500).end()
+    }).then( result => {
+        console.log(result)
+        res.json({status: true})
+    })
+})
+
+//after the route definition, listen on the port and then log to let us know the app is ready
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+//HOW COME THIS DOES NOT DO ANYTHING?
+console.log(process.env)
+
+//# npm i pg pg-hstore sequelize
